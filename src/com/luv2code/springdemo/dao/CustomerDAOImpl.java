968ec7c9add29675fc9cbc76dbbe01b52ce70c9d
Jorge <jorgearobla@gmail.com>
2020-06-04 19:08:36 +0100

    initial commit

---
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,77 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	// need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	public List<Customer> getCustomers() {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// create a query
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName", Customer.class);
+		
+		// execute a query and get result list
+		List<Customer> customers = theQuery.getResultList();
+		
+		// return the results
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer customer) {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// save or update the customer
+		currentSession.saveOrUpdate(customer);
+
+	}
+
+	@Override
+	public Customer getCustomer(int id) {
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// retrieve/read the customer from database
+		Customer customer = currentSession.get(Customer.class, id);
+		
+		// return the results
+		return customer;
+	}
+
+	@Override
+	public void deleteCustomer(int id) {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// delete the customer from database
+		// create a query
+		Query<Customer> theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
+		
+		theQuery.setParameter("customerId", id);
+		
+		theQuery.executeUpdate();
+		
+	}
+
+}
